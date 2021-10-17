1e38e0f2d93b7b07f8c0fce7ed9ae5d0e93ba4c0
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-10-17 05:42:16 +0100

    Add subnets

--- variables.tf
+++ variables.tf
@@ -4,6 +4,30 @@ variable "gcp_credentials" {
 variable "project_name" {
   type = string
 }
+variable "private_subnet_region" {
+  type = string
+}
+variable "public_subnet_region" {
+  type = string
+}
+variable "public_subnet_zone" {
+  type = string
+}
+variable "private_subnet_zone" {
+  type = string
+}
+variable "private_subnet_cidr" {
+  type = string
+}
+variable "public_subnet_cidr" {
+  type = string
+}
 variable "vpc_name" {
+  type = string
+}
+variable "private_subnet_name" {
+  type = string
+}
+variable "public_subnet_name" {
   type = string
 }
--- vpc-network.tf
+++ vpc-network.tf
@@ -8,6 +8,17 @@ resource "google_compute_network" "sca-project-network" {
 }
 
 # Create subnets
-resource "google_compute_subnetwork" "sca-project-subnet" {
-  
+resource "google_compute_subnetwork" "sca-project-private-subnet" {
+  name = "${var.private_subnet_name}"
+  ip_cidr_range = "${var.private_subnet_cidr}"
+  network = "${var.vpc_name}"
+  private_ip_google_access = true
+  region = "${var.private_subnet_region}"
 }
+resource "google_compute_subnetwork" "sca-project-public-subnet" {
+  name = "${var.public_subnet_name}"
+  ip_cidr_range = "${var.public_subnet_cidr}"
+  network = "${var.vpc_name}"
+  region = "${var.public_subnet_region}"
+}
+
