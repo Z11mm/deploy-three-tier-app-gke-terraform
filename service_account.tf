c29e47638ed466a19e408783bd035ec2aa44fc16
Zimuzo Obiechina <zimobiechina@gmail.com>
2021-10-24 06:10:27 +0100

    Add infrastructure

--- service_account.tf
+++ service_account.tf
@@ -1,4 +1,6 @@
 resource "google_service_account" "db_service_account" {
   account_id   = "cluster-sa-id"
   display_name = "Database Service Account"
+
+  description = "A service account to allow GKE cluster access the Cloud SQL database"
 }
